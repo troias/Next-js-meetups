deba947b494192e398e60521be4895b1366529d8
troy <hunter007nz@gmail.com>
2021-10-11 03:10:49 +1300

    added ssr and static site generation

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,19 +1,17 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
-
-import { getEventById } from '../../dummy-data';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
+  const {event} = props;
+  console.log("EventDetailPageProps", props)
+   // const event = getEventById(eventId);
+ 
+  
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
-  if (!event) {
+  if (!props.event) {
     return (
       <ErrorAlert>
         <p>No event found!</p>
@@ -35,6 +33,35 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+export const getStaticPaths = async  (ctx) => {
+  return {
+    paths: [
+      {params: {eventId: "e1"}},
+      {params: {eventId: "e2"}}
+    ],  fallback: false
+  }
+}
+
+export const getStaticProps = async (ctx) => {
+ 
+  const eventId = ctx.params.eventId
+  const req = await fetch(`https://next-js-example-8e3b6-default-rtdb.firebaseio.com/events.json?orderBy="$key"&equalTo="${eventId}"`)
+  const res = await req.json()
+  const event = res[`${eventId}`]
+  
+
+  return {
+    props: {
+      event: {
+        title: event.title,
+        date: event.date,
+        location: event.location,
+        image: event.image,
+        description: event.description
+      }
+    },
+  }
 }
 
 export default EventDetailPage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import { getFeaturedEvents } from '../dummy-data';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+ 
+  // const featuredEvents = getFeaturedEvents();
+  const [ featuredEvents, setFeaturedEvents] = useState([])
+ 
+  useEffect(() => {
+
+    let transformedData = []
+    for (const x in props ) {
+      transformedData.push({
+        id: x,
+        description: props[x].description,
+        image: props[x].image,
+        isFeatured: props[x].isFeatured,
+        location: props[x].location,
+        title: props[x].title,
+      })
+    }
+    setFeaturedEvents(transformedData)
+    console.log("transformedData", transformedData)
+  }, [])
+
+
 
   return (
     <div>
@@ -11,4 +33,13 @@ function HomePage() {
   );
 }
 
+export const getStaticProps = async (ctx) => {
+
+  const req = await fetch("https://next-js-example-8e3b6-default-rtdb.firebaseio.com/events.json")
+  const res = await req.json()
+
+  return {
+    props: res
+  }
+}
 export default HomePage;
